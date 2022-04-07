fd338ca37cfc956533d757a65064ea74e63664f0
Sanjay Sunil <sanjaysunil@protonmail.com>
2022-04-07 21:01:45 +0100

    init: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 13,
+  },
+  'rules': {
+    'max-len': 0,
+    'camelcase': 0,
+  },
+};
---
+++ js/terminal.js
@@ -0,0 +1,71 @@
+/* eslint-disable require-jsdoc */
+greeting = 'TermTab [Version 1.0.0]\n(c) Sanjay Sunil. All rights reserved.\n';
+
+commands = [
+  {
+    command: 'help',
+    description: 'Provides a list of commands.',
+  },
+  {
+    command: 'reboot',
+    description: 'Restarts the terminal.',
+  },
+  {
+    command: 'clear',
+    description: 'Clears the terminal.',
+  },
+  {
+    command: 'Google',
+    description: 'Use Google search engine.'
+  }
+];
+
+const term = $('body').terminal(
+    {
+      reboot: function() {
+        window.location.reload();
+      },
+      help: function() {
+        this.echo('For more information on a specific command, type help command-name');
+
+        const sorted_command_list = commands.sort(function(a, b) {
+          const nameA = a.command.toUpperCase();
+          const nameB = b.command.toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        });
+
+        sorted_command_list.forEach((i) => {
+          space_length = 10 - i.command.length;
+          space = ' ';
+          this.echo(`${i.command}${space.repeat(space_length)}${i.description}`);
+        });
+
+        this.echo('');
+      },
+      google: function(...args) {
+        str = '';
+        args.forEach((i) => {
+          str = str + '+' + i;
+        });
+        location.href = `http://www.google.com/search?q=${str.substring(1)}`;
+      },
+    },
+    {
+      checkArity: false,
+      name: 'TermTab',
+      greetings: greeting,
+      strings: {
+        commandNotFound: '%s: command not found',
+      },
+    },
+);
+
+term.set_prompt(function(set_prompt) {
+  set_prompt(`$ `);
+});
