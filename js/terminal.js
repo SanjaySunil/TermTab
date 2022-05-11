2601f621d4881c4de2db208819d34fda1c946b19
Sanjay Sunil <45319920+SanjaySunil@users.noreply.github.com>
2022-05-11 22:24:45 +0100

    feat: add drive, bing, yt and screenfetch
    drive: query google drive
    bing: use bing search engine
    yt: use yt search engine
    screenfetch: display useful system info

--- js/terminal.js
+++ js/terminal.js
@@ -1,5 +1,5 @@
 /* eslint-disable require-jsdoc */
-greeting = 'TermTab [Version 1.1.0]\n(c) Sanjay Sunil. All rights reserved.\n';
+greeting = 'TermTab [Version 1.2.0]\n(c) Sanjay Sunil. All rights reserved.\n';
 
 commands = [
   {
@@ -21,60 +21,137 @@ commands = [
   {
     command: 'ddg [query]',
     description: 'Use DuckDuckGo search engine.'
-  }
+  },
+  {
+    command: 'drive [query]',
+    description: 'Query Google Drive.'
+  },
+  {
+    command: 'bing [query]',
+    description: 'Use Bing search engine.'
+  },
+  {
+    command: 'yt [query]',
+    description: 'Use Youtube search engine.'
+  },
+  {
+    command: 'screenfetch',
+    description: 'Displays useful system information.'
+  },
 ];
 
 const term = $('body').terminal(
-    {
-      reboot: function() {
-        window.location.reload();
-      },
-      help: function() {
-        const sorted_command_list = commands.sort(function(a, b) {
-          const nameA = a.command.toUpperCase();
-          const nameB = b.command.toUpperCase();
-          if (nameA < nameB) {
-            return -1;
-          }
-          if (nameA > nameB) {
-            return 1;
-          }
-          return 0;
-        });
-        for (let i in sorted_command_list) {
-          space_length = 30 - sorted_command_list[i]['command'].length;
-          console.log(space_length)
-          space = ' ';
-          //${space.repeat(space_length)}
-          this.echo(`${sorted_command_list[i]['command']}${space.repeat(space_length)}${sorted_command_list[i]['description']}`);
+  {
+    reboot: function () {
+      window.location.reload();
+    },
+    help: function () {
+      const sorted_command_list = commands.sort(function (a, b) {
+        const nameA = a.command.toUpperCase();
+        const nameB = b.command.toUpperCase();
+        if (nameA < nameB) {
+          return -1;
         }
-        this.echo('');
-      },
-      google: function(...args) {
-        str = '';
-        args.forEach((i) => {
-          str = str + '+' + i;
-        });
-        location.href = `https://www.google.com/search?q=${str.substring(1)}`;
-      },
-      ddg: function(...args) {
-        str = '';
-        args.forEach((i) => {
-          str = str + '+' + i;
-        });
-        location.href = `https://duckduckgo.com/?q=${str.substring(1)}`;
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      });
+      for (let i in sorted_command_list) {
+        space_length = 30 - sorted_command_list[i]['command'].length;
+        console.log(space_length)
+        space = ' ';
+        //${space.repeat(space_length)}
+        this.echo(`${sorted_command_list[i]['command']}${space.repeat(space_length)}${sorted_command_list[i]['description']}`);
       }
+      this.echo('');
     },
-    {
-      checkArity: false,
-      name: 'TermTab',
-      greetings: greeting,
-      strings: {
-        commandNotFound: '%s: command not found',
-      },
+    google: function (...args) {
+      str = '';
+      args.forEach((i) => {
+        str = str + '+' + i;
+      });
+      location.href = `https://www.google.com/search?q=${str.substring(1)}`;
     },
+    ddg: function (...args) {
+      str = '';
+      args.forEach((i) => {
+        str = str + '+' + i;
+      });
+      location.href = `https://duckduckgo.com/?q=${str.substring(1)}`;
+    },
+    bing: function (...args) {
+      str = '';
+      args.forEach((i) => {
+        str = str + '+' + i;
+      });
+      location.href = `https://www.bing.com/search?q=${str.substring(1)}`;
+    },
+    drive: function (...args) {
+      str = '';
+      args.forEach((i) => {
+        str = str + '+' + i;
+      });
+      location.href = `https://drive.google.com/drive/search?q=${str.substring(1)}`;
+    },
+    yt: function (...args) {
+      str = '';
+      args.forEach((i) => {
+        str = str + '+' + i;
+      });
+      location.href = `https://www.youtube.com/results?search_query=${str.substring(1)}`;
+    },
+    screenfetch: function () {
+      navigator.sayswho = (function () {
+        var ua = navigator.userAgent;
+        var tem;
+        var M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+        if (/trident/i.test(M[1])) {
+          tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+          return 'IE ' + (tem[1] || '');
+        }
+        if (M[1] === 'Chrome') {
+          tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+          if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+        }
+        M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+        if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+        return M.join(' ');
+      })();
+
+      function getPreferredColorScheme() {
+        if (window.matchMedia) {
+          if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            return 'Dark';
+          } else {
+            return 'Light';
+          }
+        }
+        return 'light';
+      }
+
+      term.echo(
+        `\n88888888888 88888888888    root@termtab
+    888         888        ------------
+    888         888        OS: ${navigator.platform}
+    888         888        Browser: ${navigator.sayswho}
+    888         888        Resolution: ${window.screen.availWidth}x${window.screen.availWidth}
+    888         888        Language: ${navigator.language}
+    888         888        Browser Theme: ${getPreferredColorScheme()}
+    888         888\n`.replace(new RegExp(" {" + (term.version().length + 1) + "}$"), ''),
+      );
+    },
+  },
+  {
+    checkArity: false,
+    name: 'TermTab',
+    greetings: greeting,
+    strings: {
+      commandNotFound: '%s: command not found',
+    },
+  },
 );
 
-term.set_prompt(function(set_prompt) {
+term.set_prompt(function (set_prompt) {
   set_prompt(`$ `);
 });
